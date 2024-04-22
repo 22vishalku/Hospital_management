832a3a3f3b5beace11744beea391f1433e61d8f5
Vishal Kumar <114867431+22vishalku@users.noreply.github.com>
2024-04-23 02:30:02 +0530

    Update server.js

--- Backened/server.js
+++ Backened/server.js
@@ -1,5 +1,7 @@
 import app from "./app.js";
 import cloudinary from "cloudinary";
+const port = process.env.PORT || 5000;
+
 cloudinary.v2.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
